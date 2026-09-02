7d6453e23a123c3a5e4e9c18882bf04c2080f10d
CorranEveryone <corraneveryone@gmail.com>
2026-09-01 16:05:30 -1000

    Optimizations to updating Zero and Negative registers + Jump to Subroutine Instruction

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ def cpumap(address:bytearray) -> bytearray: #address:16bit
 def writecpumap(address:bytearray, value:bytearray) -> bool:
     addresspage = address[0:1]
     addressbyte = address[1:2]
-    value = value[0]
     returncode = True
     if addresspage == bytearray(b'\x00') or addresspage == bytearray(b'\x08') or addresspage == bytearray(b'\x10') or addresspage == bytearray(b'\x18'): # RAM Page 0
         ram[addressbyte[0]:addressbyte[0]+1] = value
@@ -90,34 +89,19 @@ def addTo16BitInt(givenint:bytearray, inttoadd:int) -> bytearray:
         usableint0 += 1
     return bytearray([usableint0, usableint1])
 
-def intToBin(givenint:bytearray) -> list:
-    usableint = givenint[0]
-    returnvalue = [0, 0, 0, 0, 0, 0, 0, 0]
-    if usableint >= 128:
-        returnvalue[7] = 1
-        usableint -= 128
-    elif usableint >= 64:
-        returnvalue[6] = 1
-        usableint -= 64
-    elif usableint >= 32:
-        returnvalue[5] = 1
-        usableint -= 32
-    elif usableint >= 16:
-        returnvalue[4] = 1
-        usableint -= 16
-    elif usableint >= 8:
-        returnvalue[3] = 1
-        usableint -= 8
-    elif usableint >= 4:
-        returnvalue[2] = 1
-        usableint -= 4
-    elif usableint >= 2:
-        returnvalue[1] = 1
-        usableint -= 2
-    elif usableint >= 1:
-        returnvalue[0] = 1
-        usableint -= 1
-    return returnvalue
+def updateNegativeFlag(givenint:bytearray) -> None:
+    global cpu_n
+    if signedInt(givenint) < 0:
+        cpu_n = bytearray(b'\x01')
+    else:
+        cpu_n = bytearray(b'\x00')
+
+def updateZeroFlag(givenint:bytearray) -> None:
+    global cpu_z
+    if signedInt(givenint) == 0:
+        cpu_z = bytearray(b'\x01')
+    else:
+        cpu_z = bytearray(b'\x00')
 
 def signedInt(givenint:bytearray) -> int:
     usableint = givenint[0]
@@ -140,20 +124,28 @@ def cpu(address:bytearray) -> bool:
             cpu_cycles += 1
         if address[0:1] != addTo16BitInt(address, signedInt(addTo16BitInt(address, 1))+2):
             cpu_cycles += 1
+    elif opcode == bytearray(b'\x20'): #JSR - Jump to Subroutine
+        cpu_returnpc = addTo16BitInt(address, 2)
+        i = bytearray(b'\x01')
+        i.extend(cpu_s)
+        cpu_a = writecpumap(i, cpu_returnpc[1:2])
+        cpu_s = bytearray([cpu_s[0]-1])
+        i = bytearray(b'\x01')
+        i.extend(cpu_s)
+        cpu_a = writecpumap(i, cpu_returnpc[0:1])
+        cpu_s = bytearray([cpu_s[0]-1])
+        i = cpumap(addTo16BitInt(address, 2))
+        i.extend(cpumap(addTo16BitInt(address, 1)))
+        cpu_returnpc = i
+        cpu_cycles = 6
     elif opcode == bytearray(b'\x78'): #SEI - Set Interrupt Disable
         cpu_i = bytearray(b'\x01') # PATCH_NEEDED Eventually delay by an instruction
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\x8A'): #TXA - Transfer X to A
         cpu_a = cpu_x
-        if cpu_a == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_a)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_a)
+        updateNegativeFlag(cpu_a)
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\x8D'): #STA - Store A (Absolute)
@@ -164,14 +156,8 @@ def cpu(address:bytearray) -> bool:
         cpu_cycles = 4
     elif opcode == bytearray(b'\x98'): #TYA - Transfer Y to A
         cpu_a = cpu_y
-        if cpu_a == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_a)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_a)
+        updateNegativeFlag(cpu_a)
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\x9A'): #TXS - Transfer X to Stack Pointer
@@ -180,76 +166,40 @@ def cpu(address:bytearray) -> bool:
         cpu_cycles = 2
     elif opcode == bytearray(b'\xA0'): #LDY - Load Y (#Immediate)
         cpu_y = cpumap(addTo16BitInt(address, 1))
-        if cpu_y == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_y)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_y)
+        updateNegativeFlag(cpu_y)
         cpu_returnpc = addTo16BitInt(address, 2)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xA2'): #LDX - Load X (#Immediate)
         cpu_x = cpumap(addTo16BitInt(address, 1))
-        if cpu_x == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_x)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_x)
+        updateNegativeFlag(cpu_x)
         cpu_returnpc = addTo16BitInt(address, 2)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xA8'): #TAY - Transfer A to Y
         cpu_y = cpu_a
-        if cpu_y == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_y)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_y)
+        updateNegativeFlag(cpu_y)
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xA9'): #LDA - Load A (#Immediate)
         cpu_a = cpumap(addTo16BitInt(address, 1))
-        if cpu_a == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_a)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_a)
+        updateNegativeFlag(cpu_a)
         cpu_returnpc = addTo16BitInt(address, 2)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xAA'): #TAX - Transfer A to X
         cpu_x = cpu_a
-        if cpu_x == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_x)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_x)
+        updateNegativeFlag(cpu_x)
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xAD'): #LDA - Load A (Absolute)
         i = cpumap(addTo16BitInt(address, 2))
         i.extend(cpumap(addTo16BitInt(address, 1)))
         cpu_a = cpumap(i)
-        if cpu_a == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_a)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_a)
+        updateNegativeFlag(cpu_a)
         cpu_returnpc = addTo16BitInt(address, 3)
         cpu_cycles = 4
     elif opcode == bytearray(b'\xB0'): #BCS - Branch if Carry Set
@@ -264,28 +214,16 @@ def cpu(address:bytearray) -> bool:
             cpu_cycles += 1
     elif opcode == bytearray(b'\xBA'): #TSX - Transfer Stack Pointer to X
         cpu_x = cpu_s
-        if cpu_x == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_x)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_x)
+        updateNegativeFlag(cpu_x)
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xBD'): #LDA - Load A (Absolute, X)
         i = cpumap(addTo16BitInt(address, 2))
         i.extend(cpumap(addTo16BitInt(address, 1)))
         cpu_a = cpumap(addTo16BitInt(i, cpu_x[0]))
-        if cpu_a == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_a)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_a)
+        updateNegativeFlag(cpu_a)
         cpu_returnpc = addTo16BitInt(address, 3)
         cpu_cycles = 4
         if i[0:1] != addTo16BitInt(i, cpu_x[0])[0:1]: # Account for "oops" cycle
@@ -309,14 +247,8 @@ def cpu(address:bytearray) -> bool:
         cpu_x[0] -= 1
         while cpu_x[0] < 0:
             cpu_x[0] += 256
-        if cpu_x == bytearray(b'\x00'):
-            cpu_z = bytearray(b'\x01')
-        else:
-            cpu_z = bytearray(b'\x00')
-        if intToBin(cpu_x)[7] == 1:
-            cpu_n = bytearray(b'\x01')
-        else:
-            cpu_n = bytearray(b'\x00')
+        updateZeroFlag(cpu_x)
+        updateNegativeFlag(cpu_x)
         cpu_returnpc = addTo16BitInt(address, 1)
         cpu_cycles = 2
     elif opcode == bytearray(b'\xD0'): #BPL - Branch if Plus
